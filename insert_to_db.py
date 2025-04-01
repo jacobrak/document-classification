443be802f3015f46c7fec69257354a8fca499519
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-04-01 17:40:38 +0200

    mongodb

---
+++ insert_to_db.py
@@ -0,0 +1,17 @@
+import os
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["story_database"]  # Database name
+collection = db["short_stories"]
+
+data_folder = "data"
+
+for filename in os.listdir(data_folder):
+    if filename.endswith(".txt"):
+        with open(os.path.join(data_folder, filename), "r", encoding="utf-8") as file:
+            story_text = file.read()
+
+        # Insert into MongoDB
+        collection.insert_one({"filename": filename, "story": story_text})
+print("done")
